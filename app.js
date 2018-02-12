f92abf032aab9505b282e1cf06e712bd85a98fa4
Ben Parman <benparman@gmail.com>
2018-02-12 16:14:18 -0700

    Added basic Google Maps API Call which centers map on user's location input.  Updated index.html & styles.css to reflect changes.

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 'use strict';
 //===========
-/////////////Endpoints//////////// -- Refactor these global variables (at some point)
+/////////////Endpoints//////////// -- Refactor these global variables (at some point)*****************
 const geoCodingEndpoint = 'https://maps.googleapis.com/maps/api/geocode/json';
 const mtbProjectEndpoint = 'https://www.mtbproject.com/data/get-trails';
 const wUndergroundEndpoint = 'http://api.wunderground.com/api';
 const googleMapstEndpoint = 'https://maps.googleapis.com/maps/api/js';
-const GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories';
 //=================================================================================
 //////////////API Keys////////////
 const geoCodingApiKey = 'AIzaSyB05Gh-VXpXhypmBg4R3hzZl8zFxJJYLGQ';
@@ -55,11 +54,37 @@ function generateLocationInput() {
   // console.log(locationInput);
   return `${locationInput}`;
 }
+function generateGoogleMap() {
+  let currentLocation = `{lat: ${STATE.lat}, lng: ${STATE.lon}}`;
+  let googleMapHTML = `<h3>Ride Map</h3>
+  <div id="map"></div>
+  <script>
+    function initMap() {
+      
+      let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: ${currentLocation}
+      });
+      let marker = new google.maps.Marker({
+        position: ${currentLocation},
+        map: map
+      });
+    }
+  </script>
+  <script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&callback=initMap">
+  </script>`;
+  return googleMapHTML;
+}
 //=================================================================================
 //////////HTML Renderers//////////
 function renderSearchForm() {
   let searchForm = generateLocationInput();
   $('.js-searchBox').html(searchForm);
+}
+function renderGoogleMap() {
+  let googleMapRendered = generateGoogleMap();
+  $('#js-google_map').html(googleMapRendered);
 }
 //=================================================================================
 ///////Geocoding AJAX Call////////
@@ -83,6 +108,7 @@ function getNormalGeoCoding(searchTerm) {
       console.log('GeoCoding Response');
       console.log(data);
       getMTBproject();
+      renderGoogleMap();
     }
   };
   $.ajax(settings);
@@ -116,10 +142,12 @@ function getMTBproject() {
 //=================================================================================
 ///////WUnderground AJAX Call////////  WORKING! Update to accept user inputs
 function getWUnderground() {
-  let searchType = ['alerts','conditions','forecast','history','hourly','planner','webcams'];
+  let searchType = [/*'alerts',*/'conditions','forecast'/*,'history','hourly','planner','webcams'*/];
   for (let i = 0; i<searchType.length; i++) {
+
+    //Loop provides multiple API calls based on number of active searchType's above.
     let conditionsURL = `${wUndergroundEndpoint}/${wUndergroundApiKey}`+
-                      `/${searchType[i]}/q/${STATE.latLng}.json`;
+                        `/${searchType[i]}/q/${STATE.latLng}.json`;
     let conditions = {
       url : conditionsURL,
       jsonp: 'callback',  //What does this do?
@@ -133,23 +161,6 @@ function getWUnderground() {
   console.log('WUnderground Response');
   console.log(STATE.JSONWUnderground);
 }
-
-
-///////WUnderground AJAX Call////////  WORKING! Update to accept user inputs
-// function getWUnderground() {
-//   let conditionsURL = `${wUndergroundEndpoint}/${wUndergroundApiKey}`+
-//                       `/${STATE.wUndergroundSearchType}/q/${STATE.latLng}.json`;
-//   let conditions = {
-//     url : conditionsURL,
-//     jsonp: 'callback',  //What does this do?
-//     dataType : 'jsonp',
-//     success : function(weatherData) {
-//       STATE.JSONWUnderground.conditions = weatherData;
-//       console.log('WUnderground Response');
-//       console.log(weatherData);
-//     }
-//   };
-//   $.ajax(conditions);
 //=================================================================================
 //////////Event Handlers//////////
 function handleUserInputs(){
